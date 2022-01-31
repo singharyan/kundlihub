d17643363dbf121ee44634dca329a232e713d6a0
singharyan <aryansinghtech@gmail.com>
2022-01-31 15:14:49 +0530

    first commit

---
+++ src/routes/startdt.svelte
@@ -0,0 +1,45 @@
+
+<p class="text-center text-3xl px-8 font-bold text-gray-600 mt-6">Start Your Digital Transformation</p>
+
+<div
+    class="container mb-2 mt-6 md:mt-12 flex mx-auto w-full items-center justify-center"
+>
+    <ul class="flex flex-col p-4">
+        <li class="border-gray-400 flex flex-row">
+            <div
+            class="select-none flex flex-1 items-center p-4 transition duration-500 ease-in-out transform hover:-translate-y-1 rounded-2xl border-4 hover:shadow border-gray-300"
+            >
+            <div
+                class="w-10 h-10 text-wrap text-center flex text-white text-bold flex-col rounded-md bg-orange-400 justify-center items-center mr-2 p-2"
+            >
+                icon
+            </div>
+                <div class="flex-1 pl-1 mr-16">
+                    <div class="font-medium text-gray-600">
+                        Business
+                        <br/>
+                        <p class="text-xs">Stores, Saloons, दुकान, cafe, stall, outlet, etc.</p>
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li class="border-gray-400 flex flex-row mt-4">
+            <div
+            class="select-none flex flex-1 items-center p-4 transition duration-500 ease-in-out transform hover:-translate-y-1 rounded-2xl border-4 hover:shadow border-gray-300"
+            >
+            <div
+                class="w-10 h-10 text-wrap text-center flex text-white text-bold flex-col rounded-md bg-orange-400 justify-center items-center mr-2 p-2"
+            >
+                icon
+            </div>
+                <div class="flex-1 pl-1 mr-16">
+                    <div class="font-medium text-gray-600">
+                        Individual Brand
+                        <br/>
+                        <p class="text-xs">Doctor, Lawyer, Consultant, Professional, etc.</p>
+                    </div>
+                </div>
+            </div>
+        </li>
+    </ul>
+</div>
